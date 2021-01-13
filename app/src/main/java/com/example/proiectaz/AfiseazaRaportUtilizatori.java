d10b42a1276849907baf4de2123f859daee5ef6a
Georgiana Bibiana <bibiana.geo@gmail.com>
2021-01-14 00:18:12 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/proiectaz/AfiseazaRaportUtilizatori.java
    #	app/src/main/java/com/example/proiectaz/PaginaPersonalaActivity.java
    #	app/src/main/java/com/example/proiectaz/RaportUtilizatori.java
    #	app/src/main/java/com/example/proiectaz/RegisterActivity.java

--- app/src/main/java/com/example/proiectaz/AfiseazaRaportUtilizatori.java
+++ app/src/main/java/com/example/proiectaz/AfiseazaRaportUtilizatori.java
@@ -1,5 +1,6 @@
 package com.example.proiectaz;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -14,6 +15,8 @@ import java.util.Random;
 public class AfiseazaRaportUtilizatori  extends View {
     private Map<String, Integer> source;
     private Paint paint;
+    private Paint paintFill=new Paint();
+    private Paint paintStroke=new Paint();
     private List<String> labels;
     private Random random;
 
@@ -21,60 +24,66 @@ public class AfiseazaRaportUtilizatori  extends View {
     {
         super(context);
         this.source = source;
-        this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.BLACK);
+        this.paint = new Paint();
+       // paint.setColor(Color.BLACK);
         labels = new ArrayList<>(source.keySet());
         random = new Random();
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onDraw(Canvas canvas) {
         if (source != null && source.size() > 0)
         {
-            paint.setStrokeWidth((float) (getHeight() * 0.01));
-            //calculam distanta minima pe care o lasam fata de margini
+            canvas.drawColor(R.color.colorAccentLight);
+
             float paddW = (float) (getWidth() * 0.1);
             float paddH = (float) (getHeight() * 0.1);
 
-            //calculam suprafata efectiva de lucru, pentru desenarea graficului. Scadem toate cele 4 margini.
             float availableWidth = getWidth() - 2 * paddW;
             float availableHeight = getHeight() - 2 * paddH;
 
-            //desenam axele graficului
+
             canvas.drawLine(paddW, paddH, paddW, paddH + availableHeight, paint);
             canvas.drawLine(paddW, paddH + availableHeight, paddW + availableWidth, paddH + availableHeight, paint);
 
-            //calculam valoarea maxima, o vom folosi ca sa putem calcula inaltimea fiecarei bare
-            double maxValue = calculateMaxim();
-            //calculam grosimea unei bare - din dimensiune disponibila, ar trebui sa scadem cate un spatiu ptr fiecare bara, la final impartind la nr total de valori
-            float widthOfElement = availableWidth / source.size();
-            for (int i = 0; i < labels.size(); i++) {
-                //determinam o culoare aleator
-               int color = Color.argb(100,
-                        1 + random.nextInt(254),
-                        1 + random.nextInt(254),
-                        1 + random.nextInt(254));
-                paint.setColor(color);
 
-                float x1 = paddW + i * widthOfElement;
+            double maxValue = calculateMaxim();
+
+            float widthOfElement = (float) (availableWidth / source.size() );
+            for (int i = 0; i < labels.size(); i++) {
+
+                paintFill.setStyle(Paint.Style.FILL);
+                paintFill.setColor(Color.rgb(216,114,114));
+                paintStroke.setStyle(Paint.Style.STROKE);
+                paintStroke.setColor(Color.BLACK);
+                paintStroke.setStrokeWidth(10);
+
+                float x1 =  (60+paddW + i * widthOfElement);
                 float y1 = (float) ((1 - source.get(labels.get(i)) / maxValue) * availableHeight + paddH);
                 float x2 = x1 + widthOfElement;
                 float y2 = paddH + availableHeight;
 
-                canvas.drawRect(x1, y1, x2, y2, paint);
+
+                canvas.drawRect( (x1), y1, (float) (x2*0.9), y2, paintFill);
+                canvas.drawRect( (x1), y1, (float) (x2*0.9), y2, paintStroke);
+
+
                 drawLabel(canvas, x1, widthOfElement, paddH, availableHeight, labels.get(i));
             }
         }
     }
 
     private void drawLabel(Canvas canvas, float x1, float widthOfElement, float paddH, float availableHeight, String label) {
-        paint.setColor(Color.BLACK);
-        paint.setTextSize((float) (0.2* widthOfElement));
-        float x = x1 + widthOfElement / 2;
-        float y = 1 / 2 * paddH + availableHeight;
-        canvas.rotate(270, x, y);
-        canvas.drawText(label + " - " + source.get(label), x, y, paint);
-        canvas.rotate(-270, x, y); // readucem canvasul in pozitia initiala. ce se scrie inainte de rotatie ramane in aceeasi pozitie.
+
+        paintStroke.setColor(Color.BLACK);
+        paintStroke.setStyle(Paint.Style.STROKE);
+        paintStroke.setTextSize(80);
+        float x =  (x1 + widthOfElement );
+        float y =  (1 / 2 * paddH + availableHeight);
+        canvas.rotate(360, x, y);
+        canvas.drawText(label + " - " + source.get(label), x-530, y-950, paintStroke);
+        canvas.rotate(-360, x, y);
     }
 
     private double calculateMaxim() {
